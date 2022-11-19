3de16f0ee700ea837e4c9be06a03664f95a5886c
Prateek Agarwal <114166286+agarwal66@users.noreply.github.com>
2022-11-19 13:57:31 +0000

    c1 c

--- .ccls-cache/@home@runner@CleverSoulfulDestination/main.c
+++ .ccls-cache/@home@runner@CleverSoulfulDestination/main.c
@@ -1,35 +1,22 @@
+#include <stdio.h>
 int main() {
-  int sum1=100+390;
-  int sum2=sum1+390;
-  int sum3=sum1+sum1;
+  int n, reversed = 0, remainder, original;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
 
-  printf("%d\n",sum1);
-  printf("%d\n",sum1);
-  printf("%d\n",sum1);
+    // reversed integer is stored in reversed variable
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
 
-  int x=60;
-  int y=40;
-  printf("%d\n",x>60 && x<40);
-  int myint=55;
-  float myfloat=6.66;
-  char mychar='B';
-  printf("%lu\n",sizeof(myint));
-  printf("%lu\n",sizeof(myfloat));
-  printf("%lu\n",sizeof(mychar));
+    // palindrome if orignal and reversed are equal
+    if (original == reversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
 
-  int i=0;
-  do {
-   printf("%d\n",i);
-   i++;
-  }
-    while(i<5);
-
-
-  
-    for(i = 0;i < 10;i++) {
-      if (i==4) {
-        continue;
-      }
-      printf("%d\n",i);
-    } 
-  int myage=55;
+    return 0;
+}
